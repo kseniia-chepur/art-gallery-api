a1b083455be9d47c2a382dfd3a2399dcb111a916
Kseniia Chepur <kseniia.chepur@gmail.com>
2025-02-04 22:46:55 +0200

    connect to mongodb

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 require('dotenv').config();
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 8080;
+const { PORT, MONGODB_URL } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .then(() => app.listen(PORT || 8080))
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB Atlas:', err.errmsg || err);
+    process.exit(1);
+  });
